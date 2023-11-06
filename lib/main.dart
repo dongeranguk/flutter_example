e66d766f02b474449a2a892b9f6a1e3153efe6b8
dongeranguk <rooetym@naver.com>
2023-11-07 00:29:14 +0900

    #2 - Widget 을 상하좌우로 배치하기
    `Row` 를 `Column`으로 감싸서 상하좌우 배치할 수도 있다.
    
    그리고, `Row` 아래에 다른 widget 을 붙일 수도 있다.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: const Text('Widget을 좌우로 배치하기'),
+        title: const Text('Widget을 상하좌우로 배치하기'),
       ),
       body: const Body(),
     ),
@@ -16,29 +16,41 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        Container(
-          width: 20,
-          height: 20,
-          color: Colors.red,
-          child: Text('Container 1'),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 20,
+              height: 20,
+              color: Colors.red,
+              child: Text('Container 1'),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.green,
+              child: Text('Container 2'),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.blue,
+              child: Text('Container 3'),
+            ),
+          ],
         ),
         Container(
-          width: 100,
-          height: 80,
-          color: Colors.green,
-          child: Text('Container 2'),
-        ),
-        Container(
-          width: 100,
-          height: 80,
-          color: Colors.blue,
-          child: Text('Container 3'),
-        ),
+          width: 300,
+          height: 120,
+          color: Colors.grey,
+          child: Text('Container 4'),
+        )
       ],
     );
   }
