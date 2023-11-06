85f49b32b7922da7ca469bf91aad77f7ecb6ef84
dongeranguk <rooetym@naver.com>
2023-11-06 23:43:00 +0900

    #2 - Widget 을 상하로 배치하기
    `Column` 을 생성하여 배치를 이곳 저곳으로 옮겨 보았다.
    
    `Column`은 다른 Widget들과 다르게 child가 아닌 `children` 필드를 가지고 있으며 해당 필드에 여러개의 Widget이 들어갈 수 있다.
    
    `Container` 의 경우 Center 로 감싸서 화면 중앙에 위치시킬 수 있었지만, `Column`의 경우에는 위치를 지정할 수 있는 `mainAxisAlignment`, `crossAxisAlignment` 필드를 직접 가지고 있어, 해당 필드에 값을 지정해서 상하좌우에 배치할 수 있다.

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,34 @@ void main() {
 }
 
 class Body extends StatelessWidget {
-
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 100,
+          height: 80,
+          color: Colors.red,
+          child: Text('Container 1'),
+        ),
+        Container(
+          width: 100,
+          height: 80,
+          color: Colors.green,
+          child: Text('Container 2'),
+        ),
+        Container(
+          width: 100,
+          height: 80,
+          color: Colors.blue,
+          child: Text('Container 3'),
+        ),
+      ],
+    );
   }
 }
