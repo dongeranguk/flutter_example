3eecbacd2e1625dcd2ffdb79eb4f34ba19cfd00d
dongeranguk <rooetym@naver.com>
2023-11-07 00:44:08 +0900

    #2 - Widget 을 스크롤뷰로 배치하기
    Widget 을 여러개 배치 하다 보면 화면이 커질 수 없어 오류가 발생할 수 있다.
    
    이때, 스크롤이 가능하도록 `SingleChildScrollView` 로 감싸서 오류를 방지할 수 있다.
    
    `SingleChildScrollView` 는 `scrollDirection` 으로 어느 방향으로 (`Axis.horizontal`, `Axis.vertical`)  스크롤 할 지 정할 수 있고, 스크롤 방향을 정했다면, 방향에 맞도록 widget (`Row`, `Column`) 을 지정해야 정상적으로 동작한다.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: const Text('Widget을 상하좌우로 배치하기'),
+        title: const Text('Widget을 스크롤뷰로 배치하기'),
       ),
       body: const Body(),
     ),
@@ -16,42 +16,98 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 20,
-              height: 20,
-              color: Colors.red,
-              child: Text('Container 1'),
-            ),
-            Container(
-              width: 100,
-              height: 80,
-              color: Colors.green,
-              child: Text('Container 2'),
-            ),
-            Container(
-              width: 100,
-              height: 80,
-              color: Colors.blue,
-              child: Text('Container 3'),
-            ),
-          ],
-        ),
-        Container(
-          width: 300,
-          height: 120,
-          color: Colors.grey,
-          child: Text('Container 4'),
-        )
-      ],
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+        ],
+      ),
     );
   }
 }
