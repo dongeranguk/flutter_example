7997416a4fc87b4d702a524575e76ba0d848cb66
dongeranguk <rooetym@naver.com>
2023-11-09 22:23:30 +0900

    #8 - Stack 을 사용하여 Widget 을 겹겹이 배치하기
    Container 안에 Container 를 선언하여 widget 을 겹겹이 쌓을 수도 있지만, flutter 에서 제공하는 기본 API 인 `Stack` 을 사용하면 코드가 간결해지고 유지보수 측면에서 좋을듯 하다.
    
    `Stack` 은 FIFO 방식의 자료 구조인 Stack 과 이름만 같은게 아니라, 똑같은 방식으로 먼저 배치한 widget 을 맨 위로 올린다.

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,11 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Stack(children: [
+      Container(width: 400, height: 400, color: Colors.black,),
+      Container(width: 300, height: 300, color: Colors.red,),
+      Container(width: 200, height: 200, color: Colors.blue,),
+      Container(width: 100, height: 100, color: Colors.green,),
+    ],);
   }
 }
