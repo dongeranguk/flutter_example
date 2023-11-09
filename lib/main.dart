5cb73c0ad46ca129eb50f1dba3c52a07530e8ccf
dongeranguk <rooetym@naver.com>
2023-11-09 22:55:05 +0900

    #8 - 쌓은 widget 을 배치하기
    쌓은 widget 을 배치할 때에는 각각의 widget를 배치하는게 어려워서 `Align` 이나 `Positioned` 을 감싸 쉽게 배치할 수 있다.
    
    Align 은 enum 타입이 있어서 각각의 값을 지정해줘야 하는 Positioned 보다 다루기 쉽다.

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,23 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
-      Container(width: 400, height: 400, color: Colors.black,),
-      Container(width: 300, height: 300, color: Colors.red,),
-      Container(width: 200, height: 200, color: Colors.blue,),
-      Container(width: 100, height: 100, color: Colors.green,),
-    ],);
+      Align(alignment: Alignment.topCenter,
+          child: Container(width: 100, height: 400, color: Colors.purple,)),
+      Align(
+        alignment: Alignment.center,
+        child: Container(width: 270,
+          height: 270,
+          decoration: const BoxDecoration(color: Color(0xFFC7AB29),
+            borderRadius: BorderRadius.all(Radius.elliptical(300, 140))),),
+      ),
+      Center(
+        child: Container(width: 280,
+            height: 280,
+            decoration: BoxDecoration(color: Color(0xFFE5C52A),
+              borderRadius: BorderRadius.circular(150),)),
+      ),
+      const Positioned(right: 10, bottom: 10, child: Text('항저우 아시안게임')),
+    ],
+    );
   }
 }
