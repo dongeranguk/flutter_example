8ae5a4c407a2ffaf99843e8a40aa7bf6cefaf71a
dongeranguk <rooetym@naver.com>
2023-11-10 23:48:16 +0900

    #12 - Stateless Widget 과 Stateful Widget
    `Stateless Widget` 은 상태가 절대 변경되지 않고,
    `Stateful Widget` 은 사용자가 위젯과 상호작용할 때 위젯이 변경될 수 있다.
    
    `Stateful Widget` 은 `State` 객체에 위젯의 상태를 저장하여 위젯의 모양과 상태를 분리한다.
    `setState()` 함수는 호출할 때 위젯을 다시 그리도록(build) 지시하여 변경된 상태를 보여준다.

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,86 @@ void main() {
     home: Scaffold(
       appBar: AppBar(title: Text('Stateless and Stateful')),
       body: Body(),
-
     ),
   ));
 }
 
 class Body extends StatelessWidget {
-
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Column(
+      children: [
+        ExampleStateless(),
+        ExampleStateful(index: 3),
+      ],
+    );
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  final int index;
+
+  const ExampleStateful({required this.index, super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  late int _index;
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+    textEditingController = TextEditingController();
+  }
+
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: GestureDetector(
+        onTap: () {
+          setState(
+            () {
+              if (_index == 5) {
+                _index = 0;
+                return;
+              }
+
+              _index++;
+            },
+          );
+        },
+        child: Container(
+          color: Colors.blue.withOpacity(_index/5),
+          child: Center(child: Text('$_index')),
+        ),
+      ),
+    );
   }
 }
