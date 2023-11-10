8dc8d2f0f2d3cd1490f61a9f14813cd9e343b3fc
dongeranguk <rooetym@naver.com>
2023-11-10 22:18:12 +0900

    #12 - 화면 초기화
    이번 실습을 진행하기 위해 이전 화면을 초기화한다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          body: ConstraintWidget(),
-        ),
-      ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Stateless and Stateful')),
+      body: Body(),
+
     ),
-  );
+  ));
 }
 
-class ConstraintWidget extends StatelessWidget {
-  const ConstraintWidget({super.key});
+class Body extends StatelessWidget {
+
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          color: Colors.blue,
-          width: 300,
-          height: 300,
-          child: Align(
-            alignment: Alignment.center,
-            child: Container(
-              color: Colors.red,
-              width: 200,
-              height: 200,
-            ),
-          ),
-        ),
-        Row(
-          children: [
-            Expanded(child: Container(color: Colors.yellow, child: Text('Hello, Flutter! Hello, Flutter! Hello, Flutter! Hello, Flutter! Hello, Flutter! Hello, Flutter! Hello, Flutter! Hello, Flutter! Hello, Flutter! Hello, Flutter! Hello, Flutter! '))),
-            Expanded(child: Container(color: Colors.green, child: Text('Hello, Flutter!')))
-          ],
-        ),
-      ],
-    );
+    return Placeholder();
   }
 }
