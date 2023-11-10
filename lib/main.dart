3051e4ed7a59d2001a26c38d7e0444bbb3284b73
dongeranguk <rooetym@naver.com>
2023-11-11 00:14:47 +0900

    #12 - Stateful Widget 2
    flutter docs 에서 예시로 제공하는 `Stateful Widget`을 만들어 보았다.
    
    + https://docs.flutter.dev/ui/interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text('Stateless and Stateful')),
-      body: Body(),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: FavoriteWidget(),
+      ),
     ),
-  ));
+  );
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorite = false;
+  int _favoriteCount = 40;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ExampleStateless(),
-        ExampleStateful(index: 3),
+        Image.asset('assets/images/lake.jpg'),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: EdgeInsets.all(0),
+              child: IconButton(
+                padding: EdgeInsets.all(0),
+                alignment: Alignment.centerRight,
+                icon: Icon(_isFavorite ? Icons.star : Icons.star_border, color: Colors.red[500],),
+                onPressed: _toggleFavorite,
+              ),
+            ),
+            SizedBox(
+              child: Text('$_favoriteCount'),
+            )
+          ],
+        )
       ],
     );
   }
-}
 
-class ExampleStateless extends StatelessWidget {
-  const ExampleStateless({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.red,
-      ),
-    );
-  }
-}
-
-class ExampleStateful extends StatefulWidget {
-  final int index;
-
-  const ExampleStateful({required this.index, super.key});
-
-  @override
-  State<ExampleStateful> createState() => _ExampleStatefulState();
-}
-
-class _ExampleStatefulState extends State<ExampleStateful> {
-  late int _index;
-  late TextEditingController textEditingController;
-
-  @override
-  void initState() {
-    super.initState();
-    _index = widget.index;
-    textEditingController = TextEditingController();
-  }
-
-
-  @override
-  void dispose() {
-    textEditingController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: GestureDetector(
-        onTap: () {
-          setState(
-            () {
-              if (_index == 5) {
-                _index = 0;
-                return;
-              }
-
-              _index++;
-            },
-          );
-        },
-        child: Container(
-          color: Colors.blue.withOpacity(_index/5),
-          child: Center(child: Text('$_index')),
-        ),
-      ),
-    );
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorite) {
+        _favoriteCount -= 1;
+        _isFavorite = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorite = true;
+      }
+    });
   }
 }
