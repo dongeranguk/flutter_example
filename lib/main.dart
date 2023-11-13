4f1391ff50eddbc25ac091297c2ad29bb7beff8d
dongeranguk <rooetym@naver.com>
2023-11-13 23:57:06 +0900

    #14 - Flutter 의 다양한 입력 1
    Flutter 의 다양한 입력들에 대해 알아보았다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,164 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: FavoriteWidget(),
-      ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Body(),
     ),
-  );
+  ));
 }
 
-class FavoriteWidget extends StatefulWidget {
-  const FavoriteWidget({super.key});
-
-  @override
-  State<FavoriteWidget> createState() => _FavoriteWidgetState();
-}
-
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorite = false;
-  int _favoriteCount = 40;
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Image.asset('assets/images/lake.jpg'),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              padding: EdgeInsets.all(0),
-              child: IconButton(
-                padding: EdgeInsets.all(0),
-                alignment: Alignment.centerRight,
-                icon: Icon(_isFavorite ? Icons.star : Icons.star_border, color: Colors.red[500],),
-                onPressed: _toggleFavorite,
-              ),
-            ),
-            SizedBox(
-              child: Text('$_favoriteCount'),
-            )
-          ],
-        )
+        TestCheckBox(),
+        TestRadioButton(),
+        TestSlider(),
+      ],
+    );
+  }
+}
+
+class TestCheckBox extends StatefulWidget {
+  const TestCheckBox({super.key});
+
+  @override
+  State<TestCheckBox> createState() => _TestCheckState();
+}
+
+class _TestCheckState extends State<TestCheckBox> {
+  late bool checked1 = false;
+  late bool checked2 = false;
+  late bool checked3 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+            value: checked1,
+            onChanged: (newValue) =>
+                setState(() => checkMethod(1, newValue ?? false))),
+        Checkbox(
+            value: checked2,
+            onChanged: (newValue) =>
+                setState(() => checkMethod(2, newValue ?? false))),
+        Checkbox(
+            value: checked3,
+            onChanged: (newValue) =>
+                setState(() => checkMethod(3, newValue ?? false))),
       ],
     );
   }
 
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorite) {
-        _favoriteCount -= 1;
-        _isFavorite = false;
+  checkMethod(int index, bool newValue) {
+    if (newValue != null) {
+      if (index == 1) {
+        checked1 = newValue;
+      } else if (index == 2) {
+        checked2 = newValue;
       } else {
-        _favoriteCount += 1;
-        _isFavorite = true;
+        checked3 = newValue;
       }
-    });
+    }
   }
 }
+
+class TestRadioButton extends StatefulWidget {
+  const TestRadioButton({super.key});
+
+  @override
+  State<TestRadioButton> createState() => _TestRadioButtonState();
+}
+
+class _TestRadioButtonState extends State<TestRadioButton> {
+  TestRadioValue? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          title: selectedValue == TestRadioValue.test1
+              ? Text(TestRadioValue.test1.name)
+              : null,
+          leading: Radio<TestRadioValue>(
+              value: TestRadioValue.test1,
+              groupValue: selectedValue,
+              onChanged: (newValue) =>
+                  setState(() => selectedValue = newValue!)),
+          onTap: () => setState(() {
+            if (selectedValue != TestRadioValue.test1) {
+              selectedValue = TestRadioValue.test1;
+            }
+          }),
+        ),
+        ListTile(
+          title: selectedValue == TestRadioValue.test2
+              ? Text(TestRadioValue.test2.name)
+              : null,
+          leading: Radio<TestRadioValue>(
+              value: TestRadioValue.test2,
+              groupValue: selectedValue,
+              onChanged: (newValue) =>
+                  setState(() => selectedValue = newValue!)),
+          onTap: () => setState(() {
+            if (selectedValue != TestRadioValue.test2) {
+              selectedValue = TestRadioValue.test2;
+            }
+          }),
+        ),
+        ListTile(
+          title: selectedValue == TestRadioValue.test3
+              ? Text(TestRadioValue.test3.name)
+              : null,
+          leading: Radio<TestRadioValue>(
+              value: TestRadioValue.test3,
+              groupValue: selectedValue,
+              onChanged: (newValue) =>
+                  setState(() => selectedValue = newValue!)),
+          onTap: () => setState(() {
+            if (selectedValue != TestRadioValue.test3) {
+              selectedValue = TestRadioValue.test3;
+            }
+          }),
+        ),
+      ],
+    );
+  }
+}
+
+enum TestRadioValue { test1, test2, test3 }
+
+class TestSlider extends StatefulWidget {
+  const TestSlider({super.key});
+
+  @override
+  State<TestSlider> createState() => _TestSliderState();
+}
+
+class _TestSliderState extends State<TestSlider> {
+  double selectedValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('${selectedValue.round()}'),
+        Slider(
+            label: selectedValue.round().toString(),
+            value: selectedValue,
+            divisions: 100,
+            max: 100,
+            min: 0,
+            onChanged: (newValue) => setState(() => selectedValue = newValue)),
+      ],
+    );
+  }
+}
+
+
