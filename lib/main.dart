24a90d785274fd03fdd60d4eb53b3112ce00cf5b
dongeranguk <rooetym@naver.com>
2023-11-16 00:39:36 +0900

    #18 - 외부 라이브러리 사용하기
    추가한 외부 라이브러리를 사용하여 간단한 예제를 만들어 보았다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text('Flutter에서 외부 라이브러리 사용하기'),),
+      appBar: AppBar(
+        title: Text('Flutter에서 외부 라이브러리 사용하기'),
+      ),
       body: TestWidget(),
     ),
   ));
 }
 
 class TestWidget extends StatelessWidget {
-
   const TestWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return RandomWords();
+  }
+}
+
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final words = generateWordPairs().take(5).toList();
+    final widgets = words
+        .map((word) => Text(
+              word.asCamelCase,
+              style: const TextStyle(fontSize: 30),
+            ))
+        .toList();
+
+    return Column(
+      children: widgets,
+    );
   }
 }
