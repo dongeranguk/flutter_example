e141990238ad4b51734aec44cd011b4df9394145
dongeranguk <rooetym@naver.com>
2023-11-15 23:00:51 +0900

    #16 - Callback 이벤트 구현
    `Callback` 함수는 다른 코드에 인수로서 넘겨주는 `실행가능한 코드`를 말한다. 넘겨 받은 코드는 이 콜백을 필요에 따라 즉시 실행하거나 나중에 실행할 수 있다.
    
    stateful widget 에서 함수를 선언한 뒤, stateless widget 의 생성자의 파라미터에 넘겨주었다.
    넘겨받은 위젯에서는 사용자의 입력에 따라 실행된다.
    
    또한, 콜백 함수를 입력에 따라 재사용할 수도 있어 유용하다.
    
    + https://ko.wikipedia.org/wiki/%EC%BD%9C%EB%B0%B1

--- lib/main.dart
+++ lib/main.dart
@@ -1,221 +1,66 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Body(),
+      appBar: AppBar(title: Text('Flutter의 Callback')),
+      body: TestWidget(),
     ),
   ));
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class TestWidget extends StatefulWidget {
+  const TestWidget({super.key});
+
+  @override
+  State<TestWidget> createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  int count = 1;
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        TestCheckBox(),
-        TestRadioButton(),
-        TestSlider(),
-        TestSwitch(),
-        TestSwitch2(),
-        TestPopupMenu()
-
-      ],
-    );
-  }
-}
-
-class TestCheckBox extends StatefulWidget {
-  const TestCheckBox({super.key});
-
-  @override
-  State<TestCheckBox> createState() => _TestCheckState();
-}
-
-class _TestCheckState extends State<TestCheckBox> {
-  late bool checked1 = false;
-  late bool checked2 = false;
-  late bool checked3 = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(
-            value: checked1,
-            onChanged: (newValue) =>
-                setState(() => checkMethod(1, newValue ?? false))),
-        Checkbox(
-            value: checked2,
-            onChanged: (newValue) =>
-                setState(() => checkMethod(2, newValue ?? false))),
-        Checkbox(
-            value: checked3,
-            onChanged: (newValue) =>
-                setState(() => checkMethod(3, newValue ?? false))),
-      ],
-    );
-  }
-
-  checkMethod(int index, bool newValue) {
-    if (newValue != null) {
-      if (index == 1) {
-        checked1 = newValue;
-      } else if (index == 2) {
-        checked2 = newValue;
-      } else {
-        checked3 = newValue;
-      }
-    }
-  }
-}
-
-class TestRadioButton extends StatefulWidget {
-  const TestRadioButton({super.key});
-
-  @override
-  State<TestRadioButton> createState() => _TestRadioButtonState();
-}
-
-class _TestRadioButtonState extends State<TestRadioButton> {
-  TestValue? selectedValue;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListTile(
-          title: selectedValue == TestValue.test1
-              ? Text(TestValue.test1.name)
-              : null,
-          leading: Radio<TestValue>(
-              value: TestValue.test1,
-              groupValue: selectedValue,
-              onChanged: (newValue) =>
-                  setState(() => selectedValue = newValue!)),
-          onTap: () => setState(() {
-            if (selectedValue != TestValue.test1) {
-              selectedValue = TestValue.test1;
-            }
-          }),
+        Text(
+          'Count : $count',
+          style: const TextStyle(fontSize: 30),
         ),
-        ListTile(
-          title: selectedValue == TestValue.test2
-              ? Text(TestValue.test2.name)
-              : null,
-          leading: Radio<TestValue>(
-              value: TestValue.test2,
-              groupValue: selectedValue,
-              onChanged: (newValue) =>
-                  setState(() => selectedValue = newValue!)),
-          onTap: () => setState(() {
-            if (selectedValue != TestValue.test2) {
-              selectedValue = TestValue.test2;
-            }
-          }),
-        ),
-        ListTile(
-          title: selectedValue == TestValue.test3
-              ? Text(TestValue.test3.name)
-              : null,
-          leading: Radio<TestValue>(
-              value: TestValue.test3,
-              groupValue: selectedValue,
-              onChanged: (newValue) =>
-                  setState(() => selectedValue = newValue!)),
-          onTap: () => setState(() {
-            if (selectedValue != TestValue.test3) {
-              selectedValue = TestValue.test3;
-            }
-          }),
-        ),
+        CountButton(addCount)
       ],
     );
   }
+
+  void addCount(int value) => setState(() => count = count + value);
 }
 
-enum TestValue { test1, test2, test3 }
+class CountButton extends StatelessWidget {
 
-class TestSlider extends StatefulWidget {
-  const TestSlider({super.key});
+  const CountButton(this.callback, {super.key});
 
-  @override
-  State<TestSlider> createState() => _TestSliderState();
-}
-
-class _TestSliderState extends State<TestSlider> {
-  double selectedValue = 0;
+  final Function(int) callback;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('${selectedValue.round()}'),
-        Slider(
-            label: selectedValue.round().toString(),
-            value: selectedValue,
-            divisions: 100,
-            max: 100,
-            min: 0,
-            onChanged: (newValue) => setState(() => selectedValue = newValue)),
-      ],
+    return GestureDetector(
+      onTap: () => callback.call(1),
+      onDoubleTap: () => callback.call(5),
+      onLongPress: () => callback.call(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            width: 100,
+            height: 40,
+            decoration: BoxDecoration(border: Border.all()),
+            child: Text('Up', style: TextStyle(fontSize: 20)),
+          ),
+        ],
+      ),
     );
-  }
-}
-
-
-class TestSwitch extends StatefulWidget {
-
-  const TestSwitch({super.key});
-
-  @override
-  State<TestSwitch> createState() => _TestSwitchState();
-}
-
-class _TestSwitchState extends State<TestSwitch> {
-  bool selectedValue = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Switch(value: selectedValue, onChanged: (newValue) => setState(() => selectedValue = newValue));
-  }
-}
-
-class TestSwitch2 extends StatefulWidget {
-
-  const TestSwitch2({super.key});
-
-  @override
-  State<TestSwitch2> createState() => _TestSwitch2State();
-}
-
-class _TestSwitch2State extends State<TestSwitch2> {
-  bool selectedValue = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoSwitch(value: selectedValue, onChanged: (newValue) => setState(() => selectedValue = newValue));
-  }
-}
-
-class TestPopupMenu extends StatefulWidget {
-
-  const TestPopupMenu({super.key});
-
-  @override
-  State<TestPopupMenu> createState() => _TestPopupMenu();
-}
-
-class _TestPopupMenu extends State<TestPopupMenu> {
-  TestValue selectedValue = TestValue.test1;
-
-  @override
-  Widget build(BuildContext context) {
-    return PopupMenuButton(itemBuilder: (context) {
-      return TestValue.values.map((value) => PopupMenuItem(value: selectedValue, child: Text(value.name))).toList();
-    });
   }
 }
