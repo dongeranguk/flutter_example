249dc2ad17205adabd25e5f485b736b9f282fc4d
dongeranguk <rooetym@naver.com>
2023-11-16 00:24:06 +0900

    #18 - 화면 초기화
    이번 실습을 진행하기 위해 이전 화면을 초기화한다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text('Flutter의 Callback')),
+      appBar: AppBar(title: Text('Flutter에서 외부 라이브러리 사용하기'),),
       body: TestWidget(),
     ),
   ));
 }
 
-class TestWidget extends StatefulWidget {
+class TestWidget extends StatelessWidget {
+
   const TestWidget({super.key});
 
   @override
-  State<TestWidget> createState() => _TestWidgetState();
-}
-
-class _TestWidgetState extends State<TestWidget> {
-  int count = 1;
-
-  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Count : $count',
-          style: const TextStyle(fontSize: 30),
-        ),
-        CountButton(addCount)
-      ],
-    );
-  }
-
-  void addCount(int value) => setState(() => count = count + value);
-}
-
-class CountButton extends StatelessWidget {
-
-  const CountButton(this.callback, {super.key});
-
-  final Function(int) callback;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => callback.call(1),
-      onDoubleTap: () => callback.call(5),
-      onLongPress: () => callback.call(10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(vertical: 8),
-            width: 100,
-            height: 40,
-            decoration: BoxDecoration(border: Border.all()),
-            child: Text('Up', style: TextStyle(fontSize: 20)),
-          ),
-        ],
-      ),
-    );
+    return Placeholder();
   }
 }
