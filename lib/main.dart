4426a96479c0682d3d8fe5059bcd3031b325f694
dongeranguk <rooetym@naver.com>
2023-11-16 22:30:56 +0900

    #18 - 화면 초기화
    이번 실습을 진행하기 위해 이전 화면을 초기화한다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter에서 외부 라이브러리 사용하기'),
-      ),
-      body: TestWidget(),
+      appBar: AppBar(title: const Text('Flutter에서 로컬 데이터 활용하기')),
+      body: const Body(),
     ),
   ));
 }
 
-class TestWidget extends StatelessWidget {
-  const TestWidget({super.key});
+class Body extends StatelessWidget {
+
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return RandomWords();
-  }
-}
+    return const Placeholder();
 
-class RandomWords extends StatelessWidget {
-  const RandomWords({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final words = generateWordPairs().take(5).toList();
-    final widgets = words
-        .map((word) => Text(
-              word.asCamelCase,
-              style: const TextStyle(fontSize: 30),
-            ))
-        .toList();
-
-    return Column(
-      children: widgets,
-    );
   }
 }
