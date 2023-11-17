d4f1ef01c3f0e723cb317154bf0fb2378e222378
dongeranguk <rooetym@naver.com>
2023-11-17 23:49:43 +0900

    #20 - BottomNavigateBar 사용하여 화면 전환하기
    `BottomNavigateBar` 를 사용하여 상태 관리를 통해
    화면을 전환 시킬 수 있다.
    
    BottomNavigateBar 는 items 라는 필드를 가지고 있는데, 이 필드에 `BottomNavigationBarItem` widget을  초기화해준다.
    
    `currentIndex` 는 현재 활성화된`BottomNavigationBarItem` 인덱스이고,
    `onTap` 은 items 의 인덱스를 추적하여 `currentIndex`에 다시 선택된 인덱스를 지정해주어 해당 아이템에 활성화 효과를 낼 수 있다.

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(title: const Text('Flutter에서 화면 이동하기')),
-    body: const HomeWidget()),
+    home: HomeWidget(),
   ));
 }
 
 class HomeWidget extends StatefulWidget {
-
   const HomeWidget({super.key});
 
   @override
@@ -17,9 +14,41 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter에서 화면 이동하기')),
+      body: homeBody(index),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User')
+        ],
+        currentIndex: index,
+        onTap: (newValue) => setState(() => index = newValue),
+      ),
+    );
+  }
+
+  Widget homeBody(int index) {
+    switch (index) {
+      case 0:
+        return Center(child: Icon(Icons.home, size: 200));
+      case 1:
+        return Center(child: Icon(Icons.search, size: 200));
+      case 2:
+        return Center(child: Icon(Icons.person, size: 200));
+      default:
+        return Center(child: Icon(Icons.bug_report, size: 200));
+    }
   }
 }
